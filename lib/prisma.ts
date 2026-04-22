24ba7b934e8aef325df7279b96ad39419299da44
Sunraj Mandal <sunrajmandal@users.noreply.github.com>
2026-04-22 19:43:14 +0545

    Configure production database compatibility (PostgreSQL)

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,10 @@
+import { PrismaClient } from '@prisma/client';
 import prismaMock from './db';
-export const prisma = prismaMock;
+
+const prisma = process.env.NODE_ENV === 'production' 
+  ? new PrismaClient() 
+  : prismaMock as any;
+
+export { prisma };
 export default prisma;
 
